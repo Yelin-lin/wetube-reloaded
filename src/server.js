762003409c0e4f0a1d49d152306d583bac430b7c
Yelin-lin <77089775+Yelin-lin@users.noreply.github.com>
2023-04-07 20:41:14 +0900

    7.3

--- src/server.js
+++ src/server.js
@@ -1,11 +1,18 @@
 import express from "express";
-
-const POST = 4000;
+import morgan from "morgan";
+import rootRouter from "./routers/rootRouter";
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
 
 const app = express();
+const logger = morgan("dev");
 
-const handleListening = (req, res)=> {
-    return res.end();
-};
+app.set("view engine", "pug");
+app.set("views", process.cwd() + "/src/views");
+app.use(logger);
+app.use(express.urlencoded({extended:true}));
+app.use("/", rootRouter);
+app.use("/users", userRouter);
+app.use("/videos", videoRouter);
 
-app.listen(4000, handleListening);
+export default app;
