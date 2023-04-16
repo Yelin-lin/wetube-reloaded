971dcc608bd09d4b69f4adbbc78c8724baf0fa07
Yelin-lin <77089775+Yelin-lin@users.noreply.github.com>
2023-04-16 20:08:00 +0900

    7.20

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,6 +1,6 @@
 import User from "../models/User";
+import fetch from "node-fetch";
 import bcrypt from "bcrypt";
-import session from "express-session";
 
 export const getJoin = (req, res) => res.render("join", { pageTitle: "Join" });
 export const postJoin = async (req, res) => {
@@ -63,21 +63,56 @@ export const postLogin = async(req, res) => {
   req.session.user = user;
   return res.redirect("/");
 };
-
 export const startGithubLogin = (req, res) => {
-  const baseUrl = "http://github.com/login/oauth/authorize";
+  const baseUrl = "https://github.com/login/oauth/authorize";
   const config = {
-    client_id:"20ed91ed3c0e5fa2876e",
-    allow_signup:false,
-    scope:"read:user user:email"
+    client_id: process.env.GH_CLIENT,
+    allow_signup: false,
+    scope: "read:user user:email",
   };
   const params = new URLSearchParams(config).toString();
   const finalUrl = `${baseUrl}?${params}`;
   return res.redirect(finalUrl);
 };
 
-export const finishGithubLogin = (req, res) => {
-  res.send("로그인됨!");
+export const finishGithubLogin = async (req, res) => {
+  const baseUrl = "https://github.com/login/oauth/access_token";
+  const config = {
+    client_id: process.env.GH_CLIENT,
+    client_secret: process.env.GH_SECRET,
+    code: req.query.code,
+  };
+  const params = new URLSearchParams(config).toString();
+  const finalUrl = `${baseUrl}?${params}`;
+  const tokenRequest = await (await fetch(finalUrl, {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+    },
+  })).json();
+  if("access_token" in tokenRequest){
+    const {access_token} = tokenRequest;
+    const apiUrl = "https://api.github.com"
+    const userData = await (await fetch(`${apiUrl}/user`, {
+      headers:{
+        Authorization: `token ${access_token}`,
+      },
+    })).json();
+    console.log(userData);
+    const emailData = await (await fetch(`${apiUrl}/user/emails`, {
+        headers:{
+          Authorization: `token ${access_token}`,
+        },
+      })).json();
+      const email = emailData.find(
+        (email)=>email.primary === true && email.verified === true
+      );
+      if(!email){
+        return res.redirect("/login");
+      }
+  }else{
+    return res.redirect("/login");
+  }
 };
 
 export const edit = (req, res) => res.send("Edit User");
